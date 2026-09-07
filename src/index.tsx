81465bcac7fe9d0c43b4c726d6c3cd17a29e392c
ruowu12 <1994896374@qq.com>
2026-09-07 15:29:52 +0800

    chore: 更新 Tauri 版本到 1.0.0 匹配 git tag

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,14 @@ import App from "./App";
 
 // Polyfill for File System Access API to prevent browser-fs-access errors
 if (typeof window !== "undefined") {
-	// @ts-expect-error
 	if (!window.showOpenFilePicker) {
-		// @ts-expect-error
 		window.showOpenFilePicker = async () => {
 			throw new Error(
 				"File System Access API is not supported in this environment",
 			);
 		};
 	}
-	// @ts-expect-error
 	if (!window.showSaveFilePicker) {
-		// @ts-expect-error
 		window.showSaveFilePicker = async () => {
 			throw new Error(
 				"File System Access API is not supported in this environment",
